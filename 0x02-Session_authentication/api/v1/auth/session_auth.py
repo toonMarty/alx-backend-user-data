076ef1d8470b4d740155649dcae5704a848385b8
toonMarty <martinnyaga7373@gmail.com>
2023-03-09 08:04:56 +0000

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr//bin/env python3
+"""
+This module contains an empty class
+SessionAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    The class definition for SessionAuth
+    """
+    pass
