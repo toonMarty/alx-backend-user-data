7a8fe8b0c75588e2ccf373d4cb69f5ec74e58f78
toonMarty <martinnyaga7373@gmail.com>
2023-03-16 13:30:35 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,7 +101,7 @@ def update_password() -> str:
     """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
-    new_password = request.form.get('password')
+    new_password = request.form.get('new_password')
 
     try:
         AUTH.update_password(reset_token, new_password)
